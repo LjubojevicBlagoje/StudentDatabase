e93bcf39f31aef5b3822d3d01da911c28b84615f
LjubojevicBlagoje <ljubojevicblagoje8@gmail.com>
2025-12-11 18:13:07 +1030

    Testing set up (altered filetree)

---
+++ tests/tests.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "/Users/blagojeljubojevic/Downloads/Projects/StudentDatabase/Course.h"
+#include "/Users/blagojeljubojevic/Downloads/Projects/StudentDatabase/Database.h"
+#include "/Users/blagojeljubojevic/Downloads/Projects/StudentDatabase/Enrollment.h"
+#include "/Users/blagojeljubojevic/Downloads/Projects/StudentDatabase/Student.h"
+
+int main(void) { 
+    std::cout << "compiled";
+
+}
