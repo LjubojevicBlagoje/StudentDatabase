e6c68c9751b6ebde98e0285904f13a97fe0bb44f
LjubojevicBlagoje <ljubojevicblagoje8@gmail.com>
2025-12-11 19:29:43 +1030

    Changed target name for testing to db_tests

---
+++ build/tests/CMakeFiles/db_tests.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/db_tests.dir/__/external/catch_amalgamated.cpp.o"
+  "CMakeFiles/db_tests.dir/__/external/catch_amalgamated.cpp.o.d"
+  "CMakeFiles/db_tests.dir/test_database.cpp.o"
+  "CMakeFiles/db_tests.dir/test_database.cpp.o.d"
+  "CMakeFiles/db_tests.dir/test_main.cpp.o"
+  "CMakeFiles/db_tests.dir/test_main.cpp.o.d"
+  "CMakeFiles/db_tests.dir/test_student.cpp.o"
+  "CMakeFiles/db_tests.dir/test_student.cpp.o.d"
+  "db_tests"
+  "db_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/db_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tests/CMakeFiles/db_tests.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for db_tests.
+# This may be replaced when dependencies are built.
